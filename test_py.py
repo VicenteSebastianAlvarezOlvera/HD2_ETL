aa3f30ba9e11c6036ab65fec918ab61c51e20924
Vicente Sebastian Alvarez Olvera <190300376@ucaribe.edu.mx>
2024-10-03 16:47:35 -0500

    Added test script to test connection to DB

---
+++ test_py.py
@@ -0,0 +1,22 @@
+import pyodbc
+import os 
+from dotenv import load_dotenv
+
+ip = os.getenv('IP')
+database = os.getenv('DB_NAME')
+uid = os.getenv('DB_USER')
+password = os.getenv('DB_PASSWORD')
+
+conn = pyodbc.connect(
+    'DRIVER={ODBC Driver 17 for SQL Server};'
+    f'SERVER={ip};'
+    f'DATABASE={database};'
+    f'UID={uid};'
+    f'PWD={password}!'
+)
+
+
+cursor = conn.cursor()
+cursor.execute("SELECT @@version;")
+row = cursor.fetchone()
+print(row)
